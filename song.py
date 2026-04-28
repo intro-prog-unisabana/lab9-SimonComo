1cf709819f0e249fe4a39a8a17fd1a7ee78548e2
Simon Correa <simoncorream@hotmail.com>
2026-04-28 11:18:44 -0500

    songpy ahora si

--- song.py
+++ song.py
@@ -3,12 +3,8 @@
 class Song:
     def __init__(self, name, artist, length):
         self.name = name
-        self.artist = artist 
-        self.length = float(length)
+        self.artist = artist
+        self.length = length
 
     def get_length_in_seconds(self):
-            return self.length * 60
-        
-my_song = Song("bohemian rapsody", "queen", 5.55)
-print(my_song.get_length_in_seconds())
-        
+        return self.length * 60
